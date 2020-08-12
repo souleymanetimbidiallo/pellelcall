0975b0315f3d2ef28a811dcbbb543d809f730335
Souleymane Diallo <49892990+souleymanetimbidiallo@users.noreply.github.com>
2020-08-12 11:02:30 +0200

    Modification de la base de  données et mise en place de l'admin

--- PellelCall/meeting/admin.py
+++ PellelCall/meeting/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.utils.text import Truncator
 
-# Register your models here.
+from .models import Profile, Offer, Souscription, Conference
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display   = ('user', 'birthdate', 'address', 'avatar')
+    list_filter    = ('address',)
+    search_fields  = ('user', 'address')
+    fields = ('user', 'birthdate', 'address', 'avatar')
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display   = ('title', 'description', 'price')
+    list_filter    = ('users', 'price' )
+    search_fields  = ('title', 'description')
+    fields = ('title', 'description', 'price',)
+
+class SouscriptionAdmin(admin.ModelAdmin):
+    list_display   = ('user', 'offer', 'souscriptionDate')
+    list_filter    = ('offer',)
+    ordering       = ('souscriptionDate', )
+    search_fields  = ('user', 'offer')
+    fields = ('user', 'offer', 'souscriptionDate')
+    
+class ConferenceAdmin(admin.ModelAdmin):
+    list_display   = ('title', 'beginDate', 'endDate', 'initiator')
+    list_filter    = ('title',)
+    search_fields  = ('title', 'description')
+    fields = ('title', 'description',  'beginDate', 'endDate', 'initiator', 'participants')
+
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Offer, OfferAdmin)
+admin.site.register(Souscription, SouscriptionAdmin)
+admin.site.register(Conference, ConferenceAdmin)
--- PellelCall/meeting/models.py
+++ PellelCall/meeting/models.py
@@ -7,13 +7,19 @@ from django.utils import timezone
 # Creation des modèles.
 
 class Profile(models.Model):
-    """ Modèle qui élargit le profil de l'utilisateur """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    """ Ce modèle élargit le profil de l'utilisateur avec d'autres champs """
+    # Fields
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Utilisateur")
     birthdate = models.DateField(verbose_name="date de naissance", null=True, blank=True)
     address = models.CharField(max_length=100, blank=True, null=True, verbose_name="adresse")
     avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
+    # Metadata
+    class Meta:
+        verbose_name = "profil"
+        ordering = ['user']
 
     def __str__(self):
+        """Retourne une chaîne de caractère pour identifier l'instance de la classe d'objet."""
         return 'Profile de: {}'.format(self.user.username)
 
 # Envoi d'un signal pour la création du profil élargit de User
@@ -28,28 +34,44 @@ def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
 class Offer(models.Model):
+    """Ce modèle représente une offre à l'utilisateur"""
     title = models.CharField(max_length=255, verbose_name="titre")
     description = models.TextField()
     price = models.DecimalField(verbose_name="prix", max_digits=10, decimal_places=2)
-    users = models.ManyToManyField(User, through="Souscription", verbose_name="Utilisateurs souscrits")
+    users = models.ManyToManyField(User, through="Souscription", verbose_name="Utilisateurs souscrits", related_name="offers")
     
+    #Metadonnées
+    class Meta:
+        verbose_name = "offre"
+        
     def __str__(self):
+        """Retourne une chaine de caractère qui répresente le titre de l'offre"""
         return self.title
 
 class Souscription(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
+    """Ce modèle représente la liaison entre une offre et un utilisateur"""
+    user = models.ForeignKey(User, related_name='souscriptions', on_delete=models.SET_NULL, null=True, verbose_name="utilisateur")
+    offer = models.ForeignKey(Offer, related_name='souscriptions',on_delete=models.SET_NULL, null=True, blank=True, verbose_name="offre")
     souscriptionDate = models.DateTimeField(default=timezone.now, verbose_name="Date de souscription")
     
     def __str__(self):
-        return "{0} a souscrit à {1} le {2} ".format(self.user, self.offer, self.souscriptionDate)
+        """Identifie l'offre d'un utilisateur à une date"""
+        return "{0} a souscrit à {1} le {2} ".format(self.user.username, self.offer, self.souscriptionDate)
 
 class Conference(models.Model):
+    """Ce modèle représente une conférence"""
     title = models.CharField(max_length=255, verbose_name="titre")
     description = models.TextField()
     secret = models.CharField(max_length=255, verbose_name="Code secret")
     beginDate = models.DateTimeField(default=timezone.now, verbose_name="Date de début")
     endDate = models.DateTimeField(default=timezone.now, verbose_name="Date de fin")
-    initiator = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Initiateur")
-    participants = models.ManyToManyField(User, related_name="+")
+    initiator = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Initiateur", related_name="+")
+    participants = models.ManyToManyField(User, related_name='conferences')
 
+    class Meta:
+        verbose_name = "conférence"
+        ordering = ['-beginDate']
+
+    def __str__(self):
+        """Retourne une chaine qui indique la conférence et l'horaire"""
+        return "{0} de {1} à {2} ".format(self.title, self.beginDate, self.endDate)
